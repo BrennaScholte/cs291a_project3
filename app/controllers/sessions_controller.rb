28e3a592d66bfe1cc69d20307b95e489ef76b12b
Brenna Scholte <brennascholte@ucsb.edu>
2024-10-29 10:53:13 -0700

    created require login, but login no longer working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
+  # before_action :set_current_user
+  before_action :require_login, except: [:new]
+
   def index
-    @session = Session.new
+    # @session = Session.new
   end
 
   def show
     @current_user = User.find_by(id: session[:user_id])
   end
 
+  def current_user
+    @current_user
+  end
+
   def new
-    @session = Session.new
+    
   end
 
   def create
@@ -22,8 +29,16 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def require_login
+    unless current_user != nil 
+      redirect_to login_path 
+    end
+  end
+
   private
     def session_params
       params.require(:session).permit(:username)
     end
+
+  
 end
