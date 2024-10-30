21bc16bf122e2432a1d2146ab228a2882fa59538
Brenna Scholte <brennascholte@ucsb.edu>
2024-10-30 13:52:26 -0700

    now able to view posts on root when logged in; added validations to post body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
     belongs_to :user
+
+    # Validations
+    validate :body_blacklist
+
+    # TODO: add more blacklisted words
+    BLACKLIST = ["Trump", "Donald Trump", "MAGA", "Kamala", "Kamala Harris", "election"]
+    private
+    def body_blacklist
+        if BLACKLIST.any? { |word| body&.include?(word) }
+            errors.add(:body, "Post may potentially influence the election. Not allowed")
+        end
+    end
 end
